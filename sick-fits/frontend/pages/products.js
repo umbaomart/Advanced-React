05c57fd36709d422ac6a2cacd05f7aee1060b6b1
ron-sympher <umbaomart@gmail.com>
2022-08-06 23:59:46 +0800

    initial commit lesson 1-12

---
+++ sick-fits/frontend/pages/products.js
@@ -0,0 +1,9 @@
+import Page from '../components/Page';
+
+export default function ProductsPage() {
+  return (
+    <div>
+      <p>Hello</p>
+    </div>
+  );
+}
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,9 @@
+import Page from '../components/Page';
+
+export default function SellPage() {
+  return (
+    <div>
+      <h1>I am the sell page</h1>
+    </div>
+  );
+}
